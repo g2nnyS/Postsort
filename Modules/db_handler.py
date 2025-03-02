bab4fffa7b3e8634bdd21d3bc35d41f8f55fee6e
Wang Xiaomei <XianYuPigeon@outlook.com>
2025-03-03 02:06:20 +0800

    feat: 添加数据库连接池以优化数据库连接管理

--- Modules/db_handler.py
+++ Modules/db_handler.py
@@ -1,7 +1,29 @@
+'''
+Date: 2025-03-03 00:06:08
+LastEditors: Wang Xiaomei XianYuPigeon@outlook.com
+LastEditTime: 2025-03-03 02:03:35
+FilePath: /Postsort/Modules/db_handler.py
+'''
 # -*- coding: UTF-8 -*-
 import pymysql
 import logging
+from dbutils.pooled_db import PooledDB
 from Modules.config_loader import ConfigLoader
+
+def create_pool(db_config):
+    return PooledDB(
+        creator=pymysql,
+        maxconnections=5,
+        mincached=2,
+        maxcached=5,
+        blocking=True,
+        host=db_config["host"],
+        port=db_config["port"],
+        user=db_config["user"],
+        password=db_config["password"],
+        database=db_config["name"],
+        charset='utf8mb4'
+    )
 
 def create_database_if_not_exists(db_config):
     logging.debug(f"正在检查数据库 {db_config['name']} 是否存在")
@@ -28,14 +50,11 @@ def insert_post(title, description, published, link, tag):
     # 创建数据库（如果不存在）
     create_database_if_not_exists(db_config)
 
-    # 连接数据库
-    conn = pymysql.connect(
-        host=db_config["host"],
-        port=db_config["port"],
-        user=db_config["user"],
-        password=db_config["password"],
-        database=db_config["name"]
-    )
+    # 创建连接池
+    pool = create_pool(db_config)
+
+    # 从连接池获取连接
+    conn = pool.connection()
     cursor = conn.cursor()
 
     # 创建表（如果不存在）
